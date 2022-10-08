66d9d7c4878617bdf8ae65f568d9d9c65e36575f
cbryanalfonso <cbryanalfonso@gmail.com>
2022-10-08 14:34:57 -0500

    Add: New Colors

--- app/utils/styles/colors.js
+++ app/utils/styles/colors.js
@@ -1,6 +1,12 @@
 const backGroundLogin = "#1F2E44";
+const backgroundColorProfile = "#242B36"
+const blanco = "#FFF";
+const colorBlueContorno = "#38B5A6";
 
 
 export{
-    backGroundLogin
+    blanco,
+    backGroundLogin,
+    backgroundColorProfile,
+    colorBlueContorno
 }
