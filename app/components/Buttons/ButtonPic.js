0f3333882249accc1ec9e33f619692a75400cc84
cbryanalfonso <cbryanalfonso@gmail.com>
2022-10-08 17:09:44 -0500

    UPD: Button Pic user

--- app/components/Buttons/ButtonPic.js
+++ app/components/Buttons/ButtonPic.js
@@ -1,19 +1,31 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { Avatar } from "@rneui/themed";
-import { widthPercentageToDP as wp} from 'react-native-responsive-screen';
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import useProfile from '../../CustomHooks/useProfile/useProfile';
+import { colorBlueContorno } from '../../utils/styles/colors';
 
-const ButtonPic = ({onPress}) => {
+const ButtonPic = ({ onPress }) => {
+  const { picProfile } = useProfile()
   return (
-    <TouchableOpacity 
-    onPress={onPress}
-    style={styles.container}
-    activeOpacity={1}
+    <TouchableOpacity
+      onPress={onPress}
+      style={styles.container}
+      activeOpacity={1}
     >
-        <Avatar
+      {
+        picProfile ? (
+          <Avatar
+            rounded
             size={'small'}
-            source={{uri:'https://cdn.pixabay.com/photo/2021/06/07/13/46/user-6318008_960_720.png'}}
-        />
+            source={{
+              uri:
+                picProfile ? picProfile :
+                  'https://cdn.pixabay.com/photo/2021/06/07/13/46/user-6318008_960_720.png'
+            }}
+          />
+        ) : <ActivityIndicator size={'small'} color={colorBlueContorno} />
+      }
     </TouchableOpacity>
   )
 }
@@ -21,12 +33,12 @@ const ButtonPic = ({onPress}) => {
 export default ButtonPic
 
 const styles = StyleSheet.create({
-    container:{
-        width: wp(10),
-        height: wp(10),
-        backgroundColor: '#38B5A6',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: wp(10)/2
-    }
+  container: {
+    width: wp(9.5),
+    height: wp(9.5),
+    backgroundColor: '#38B5A6',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: wp(10) / 2
+  }
 })
